3d3c1947bdd9853840baaecb134c745b38007474
KimYeongJun <121996068+KimYeongJun-4481@users.noreply.github.com>
2023-01-16 22:42:09 +0900

    device 옵션 설정
    Signed-off-by: KimYeongJun <121996068+KimYeongJun-4481@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import torch
 import argparse
 from pathlib import Path
 
@@ -20,9 +21,21 @@ def main():
               "book"       : [46, 240, 49]}
     classes = list(colors.keys()) # 클래스들의 영어 이름
     class_names = ["사람", "TV", "노트북", "휴대폰", "책"] # 클래스들의 한글 이름
+        
+    # 만약 세가지 옵션 중 하나라도 선택하지 않으면 예외 발생
+    assert not (args.img is None and args.source is None \
+        and args.webcam is None), "Few arguments to execute program"
+
+    if args.device == "cuda": # cuda를 사용할 경우
+        assert torch.cuda.is_available, "CUDA is not available" # cuda가 사용 불가능하다면
+        device = torch.device("cuda:0")
+    elif args.device == "mac": 
+        device = torch.device("mps") # m1 mac
+    else:
+        device = torch.device("cpu") # cpu
     
     if args.img is not None: # 하나의 이미지
-        pass
+        pass                
     elif args.source is not None: # 폴더(여러개의 이미지)
         pass
     elif args.webcam is not None: # 실시간 웹캠
